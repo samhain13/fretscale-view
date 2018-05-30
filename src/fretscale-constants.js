834f856eaa7a1960bbe9fe994537306f000f3e51
Arielle B Cruz <arielle.cruz@gmail.com>
2018-05-30 08:09:31 +0800

    added FretScale skeleton and constants

---
+++ src/fretscale-constants.js
@@ -0,0 +1,42 @@
+const PITCH_NAMES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+export {PITCH_NAMES};
+
+const FORMULAE = [
+    {
+        'name': 'Major / Ionian',
+         'items': [0, 2, 4, 5, 7, 9, 11]
+     },
+     {
+        'name': 'Natural Minor / Aeolian',
+         'items': [0, 2, 3, 5, 7, 8, 10]
+     },
+     {
+        'name': 'Harmonic Minor',
+         'items': [0, 2, 3, 5, 7, 8, 11]
+     },
+     {
+        'name': 'Jazz Minor',
+         'items': [0, 2, 3, 5, 7, 9, 11]
+     },
+     {
+        'name': 'Dorian Mode',
+         'items': [0, 2, 3, 5, 7, 9, 10]
+     },
+     {
+        'name': 'Phrygian Mode',
+         'items': [0, 1, 3, 5, 7, 8, 10]
+     },
+     {
+        'name': 'Lydian Mode',
+         'items': [0, 2, 4, 6, 7, 9, 11]
+     },
+     {
+        'name': 'Mixolydian Mode',
+         'items': [0, 2, 4, 5, 7, 9, 10]
+     },
+     {
+        'name': 'Locrian Mode',
+         'items': [0, 1, 3, 5, 6, 8, 10]
+     }
+];
+export {FORMULAE};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {PITCH_NAMES} from './fretscale-constants.js';
+import {FORMULAE} from './fretscale-constants.js';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+class FretScale extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pitch: PITCH_NAMES[0],
+            key_mode: FORMULAE[0],
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <section id="fretboard">
+                    <h2>Fretboard</h2>
+                </section>
+                
+                <section id="tuning">
+                    <h2>Tuning</h2>
+                </section>
+                
+                <section id="key-settings">
+                    <h2>Key/Mode Settings</h2>
+                </section>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <FretScale />,
+    document.getElementById('root')
+);
