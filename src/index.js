06387c601fea128feebb30b7ab389bd39a985842
Arielle B Cruz <arielle.cruz@gmail.com>
2018-05-31 15:06:34 +0800

    added pitch and key/mode settings methods to app

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ class Fretboard extends React.Component {
     render() {
         return(
             <section id="fretboard">
-                <h2>Fretboard</h2>
+                <h2>
+                    <small>Fretboard</small>
+                    <br />
+                    {this.props.fretboard_title}
+                </h2>
             </section>
         );
     }
@@ -32,6 +36,26 @@ class KeyModeSettings extends React.Component {
         return (
             <section id="key-settings">
                 <h2>Key/Mode Settings</h2>
+                <form action="." method="get">
+                    <select id="choose-pitch" name="choose-pitch">
+                    {PITCH_NAMES.map((name, index) =>
+                        <option
+                            key={index}
+                            onClick={() => this.props.app.applyPitchNameSettings(index)}>
+                            {name}
+                        </option>
+                    )}
+                    </select>
+                    <select id="choose-mode" name="choose-mode">
+                    {FORMULAE.map((item, index) => 
+                        <option
+                            key={index}
+                            onClick={() => this.props.app.applyKeyModeSettings(item)}>
+                            {item.name}
+                        </option>
+                     )}
+                    </select>
+                 </form>
             </section>
         );
     }
@@ -42,18 +66,39 @@ class FretScaleApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            pitch: PITCH_NAMES[0],
+            pitch_index: 0,
+            pitch_name: PITCH_NAMES[0],
             key_mode: FORMULAE[0],
             tuning: ['E', 'A', 'D', 'G', 'B', 'E'],
         }
+    }
+    
+    applyKeyModeSettings(formula) {
+        this.setState({key_mode: formula});
+    }
+    
+    applyPitchNameSettings(index) {
+        this.setState({
+            pitch_index: index,
+            pitch_name: PITCH_NAMES[index],
+        })
     }
 
     render() {
         return(
             <div id="fretscale-app">
-                <Fretboard />
-                <Tuning />
-                <KeyModeSettings />
+                <Fretboard
+                    app={this}
+                    fretboard_title={
+                        this.state.pitch_name + ' ' + this.state.key_mode.name
+                    }
+                />
+                <Tuning
+                    app={this}
+                />
+                <KeyModeSettings
+                    app={this}
+                />
             </div>
         );
     }
