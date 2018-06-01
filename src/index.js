52f00e982ee8355a75171550cc4640eb9e6ef430
Arielle B Cruz <arielle.cruz@gmail.com>
2018-06-01 10:59:47 +0800

    code cleanup on index.js and fretscale-constants.js

--- src/index.js
+++ src/index.js
@@ -1,272 +1,268 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {PITCH_NAMES} from './fretscale-constants.js';
-import {FORMULAE} from './fretscale-constants.js';
-import './index.css';
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {PITCH_NAMES, FORMULAE} from './fretscale-constants.js'
+import './index.css'
 
 class InstrumentString extends React.Component {
-    getFretClassName(pitch_name) {
-        let index = this.props.app.state.active_pitches.indexOf(pitch_name);
-        if (index === 0) {
-            return 'root-pitch';
-        } else if (index > 0) {
-            return 'active-pitch';
-        } else {
-            return 'inactive-pitch';
-        }
+  getFretClassName (pitchName) {
+    let index = this.props.app.state.active_pitches.indexOf(pitchName)
+    if (index === 0) {
+      return 'root-pitch'
+    } else if (index > 0) {
+      return 'active-pitch'
+    } else {
+      return 'inactive-pitch'
     }
+  }
 
-    renderTuner(pitch_name, fret_index, index) {
-        let true_index = this.props.app.state.tuning.length - index - 1;
+  renderTuner (pitchName, fretIndex, index) {
+    let trueIndex = this.props.app.state.tuning.length - index - 1
 
-        return(
-            <form action="." method="get" className="tuner-form">
-                <select
-                    id={'tuner-' + true_index}
-                    name="tuner"
-                    value={pitch_name}
-                    onChange={() => {}}  // Just to get rid of the warning.
-                >
-                {PITCH_NAMES.map((name, pitch_index) =>
-                    <option
-                        key={pitch_index}
-                        onClick={
-                            () => this.props.app.applyTuning(true_index, name)
-                        }
-                    >
-                        {name}
-                    </option>
-                )}
-                </select>
-                <input
-                    type="button"
-                    value="X"
-                    onClick={
-                        () => this.props.app.removeTuning(true_index)
-                    }
-                />
-            </form>
-        );
-    }
+    return (
+      <form action='.' method='get' className='tuner-form'>
+        <select
+          id={'tuner-' + trueIndex}
+          name='tuner'
+          value={pitchName}
+          onChange={() => {}} // Just to get rid of the warning.
+        >
+          {PITCH_NAMES.map((name, pitchIndex) =>
+            <option
+              key={pitchIndex}
+              onClick={
+                () => this.props.app.applyTuning(trueIndex, name)
+              }
+            >
+              {name}
+            </option>
+          )}
+        </select>
+        <input
+          type='button'
+          value='X'
+          onClick={
+            () => this.props.app.removeTuning(trueIndex)
+          }
+        />
+      </form>
+    )
+  }
 
-    render() {
-        let frets = this.props.app.getPitchArray(this.props.pitch);
+  render () {
+    let frets = this.props.app.getPitchArray(this.props.pitch)
 
-        return(
-            <tr key={'string-' + this.props.index}>
-            {frets.map((pitch_name, fret_index) =>
-                <td
-                    key={'fret-' + this.props.index + '-' + fret_index}
-                    className={this.getFretClassName(pitch_name)}
-                >
-                    {
-                        (fret_index === 0) ?
-                        this.renderTuner(pitch_name, fret_index, this.props.index) :
-                        pitch_name
-                    }
-                </td>
-            )}
-            </tr>
-        );
-    }
+    return (
+      <tr key={'string-' + this.props.index}>
+        {frets.map((pitchName, fretIndex) =>
+          <td
+            key={'fret-' + this.props.index + '-' + fretIndex}
+            className={this.getFretClassName(pitchName)}
+          >
+            {
+              (fretIndex === 0)
+                ? this.renderTuner(pitchName, fretIndex, this.props.index)
+                : pitchName
+            }
+          </td>
+        )}
+      </tr>
+    )
+  }
 }
-
 
 class Fretboard extends React.Component {
-    getMarkerValue(fret_number) {
-        if ([3, 5, 7, 9].indexOf(fret_number) >= 0) {
-            return '•';
-        } else if (fret_number === 12) {
-            return '••';
-        } else {
-            return '';
-        }
+  getMarkerValue (fretNumber) {
+    if ([3, 5, 7, 9].indexOf(fretNumber) >= 0) {
+      return '•'
+    } else if (fretNumber === 12) {
+      return '••'
+    } else {
+      return ''
+    }
+  }
+
+  renderMarkers () {
+    let frets = Array(Number(this.props.app.props.show_frets)).fill(null)
+
+    return (
+      <tr>
+        {frets.map((value, index) =>
+          <td key={index} className='fret-marker'>
+            {this.getMarkerValue(index)}
+          </td>
+        )}
+      </tr>
+    )
+  }
+
+  render () {
+    let strings = this.props.app.state.tuning.slice().reverse()
+    let stringComponents = []
+    for (let i = 0; i < strings.length; i++) {
+      stringComponents.push(
+        <InstrumentString
+          key={'string-' + i}
+          app={this.props.app}
+          pitch={strings[i]}
+          index={i}
+        />
+      )
     }
 
-    renderMarkers() {
-        let frets = Array(Number(this.props.app.props.show_frets)).fill(null);
-
-        return(
-            <tr>
-            {frets.map((value, index) =>
-                <td key={index} className='fret-marker'>
-                    {this.getMarkerValue(index)}
-                </td>
-            )}
-            </tr>
-        );
-    }
-
-    render() {
-        let strings = this.props.app.state.tuning.slice().reverse();
-        let string_components = [];
-        for (let i=0; i<strings.length; i++) {
-            string_components.push(
-                <InstrumentString
-                    key={'string-' + i}
-                    app={this.props.app}
-                    pitch={strings[i]}
-                    index={i}
-                />
-            );
-        }
-
-        return(
-            <section id="fretboard">
-                <h2>
-                    <small>Fretboard</small>
-                    <br />
-                    {this.props.fretboard_title}
-                </h2>
-                <table>
-                    <tbody>
-                    {this.renderMarkers()}
-                    {string_components}
-                    {this.renderMarkers()}
-                    </tbody>
-                </table>
-            </section>
-        );
-    }
+    return (
+      <section id='fretboard'>
+        <h2>
+          <small>Fretboard</small>
+          <br />
+          {this.props.fretboard_title}
+        </h2>
+        <table>
+          <tbody>
+            {this.renderMarkers()}
+            {stringComponents}
+            {this.renderMarkers()}
+          </tbody>
+        </table>
+      </section>
+    )
+  }
 }
-
 
 class KeyModeSettings extends React.Component {
-    render() {
-        return (
-            <section id="key-settings">
-                <h2>Key/Mode Settings</h2>
-                <form action="." method="get">
-                    <div className="form-group">
-                        <label htmlFor="choose-Key">Key</label>
-                        <br />
-                        <select id="choose-key" name="choose-key">
-                        {PITCH_NAMES.map((name, index) =>
-                            <option
-                                key={index}
-                                onClick={() =>
-                                this.props.app.applyPitchNameSettings(index)}
-                            >
-                                {name}
-                            </option>
-                        )}
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="choose-mode">Mode</label>
-                        <br />
-                        <select id="choose-mode" name="choose-mode">
-                        {FORMULAE.map((item, index) => 
-                            <option
-                                key={index}
-                                onClick={() =>
-                                this.props.app.applyKeyModeSettings(item)}
-                            >
-                                {item.name}
-                            </option>
-                         )}
-                        </select>
-                    </div>
-                 </form>
-            </section>
-        );
-    }
+  render () {
+    return (
+      <section id='key-settings'>
+        <h2>Key/Mode Settings</h2>
+        <form action='.' method='get'>
+          <div className='form-group'>
+            <label htmlFor='choose-Key'>Key</label>
+            <br />
+            <select id='choose-key' name='choose-key'>
+              {PITCH_NAMES.map((name, index) =>
+                <option
+                  key={index}
+                  onClick={
+                    () => this.props.app.applyPitchNameSettings(index)
+                  }
+                >
+                  {name}
+                </option>
+              )}
+            </select>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='choose-mode'>Mode</label>
+            <br />
+            <select id='choose-mode' name='choose-mode'>
+              {FORMULAE.map((item, index) =>
+                <option
+                  key={index}
+                  onClick={
+                    () => this.props.app.applyKeyModeSettings(item)
+                  }
+                >
+                  {item.name}
+                </option>
+              )}
+            </select>
+          </div>
+        </form>
+      </section>
+    )
+  }
 }
-
 
 class FretScaleApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            active_pitches: [],
-            key_mode: FORMULAE[0],
-            pitch_name: PITCH_NAMES[0],
-            tuning: ['E', 'A', 'D', 'G', 'B', 'E'],
-        }
+  constructor (props) {
+    super(props)
+    this.state = {
+      active_pitches: [],
+      key_mode: FORMULAE[0],
+      pitch_name: PITCH_NAMES[0],
+      tuning: ['E', 'A', 'D', 'G', 'B', 'E']
     }
-    
-    applyKeyModeSettings(formula) {
-        this.setState({
-            active_pitches: this.getActivePitches(
-                this.state.pitch_name,
-                formula
-            ),
-            key_mode: formula
-        });
-    }
-    
-    applyPitchNameSettings(index) {
-        this.setState({
-            active_pitches: this.getActivePitches(
-                PITCH_NAMES[index],
-                this.state.key_mode
-            ),
-            pitch_name: PITCH_NAMES[index],
-        })
-    }
+  }
 
-    applyTuning(index, pitch_name) {
-        let tuning = this.state.tuning.slice();
-        tuning[index] = pitch_name;
-        this.setState({tuning: tuning});
-    }
-    
-    componentDidMount() {
-        this.setState({
-            active_pitches: this.getActivePitches(
-                this.state.pitch_name,
-                this.state.key_mode
-            ) 
-        });
-    }
+  applyKeyModeSettings (formula) {
+    this.setState({
+      active_pitches: this.getActivePitches(
+        this.state.pitch_name,
+        formula
+      ),
+      key_mode: formula
+    })
+  }
 
-    getActivePitches(pitch_name, formula) {
-        let pitches = this.getPitchArray(pitch_name);
-        let active_pitches = [];
-        for (let i=0; i < formula.items.length; i++ ) {
-            active_pitches.push(pitches[formula.items[i]]);
-        }
-        return active_pitches;
-    }
-    
-    getPitchArray(pitch_name) {
-        let pitches = []
-        let limit = PITCH_NAMES.length;
-        let j = PITCH_NAMES.indexOf(pitch_name);
-        for (let i=0; i<this.props.show_frets; i++) {
-            let num = i + j;
-            if (num >= limit) num = num - limit;
-            pitches.push(PITCH_NAMES[num]);
-        }
-        return pitches;
-    }
+  applyPitchNameSettings (index) {
+    this.setState({
+      active_pitches: this.getActivePitches(
+        PITCH_NAMES[index],
+        this.state.key_mode
+      ),
+      pitch_name: PITCH_NAMES[index]
+    })
+  }
 
-    removeTuning(index) {
-        let tuning = this.state.tuning.slice();
-        tuning.splice(index, 1);
-        this.setState({tuning: tuning});
-    }
+  applyTuning (index, pitchName) {
+    let tuning = this.state.tuning.slice()
+    tuning[index] = pitchName
+    this.setState({tuning: tuning})
+  }
 
-    render() {
-        return(
-            <div id="fretscale-app">
-                <Fretboard
-                    app={this}
-                    fretboard_title={
-                        this.state.pitch_name + ' ' + this.state.key_mode.name
-                    }
-                />
-                <KeyModeSettings app={this} />
-            </div>
-        );
+  componentDidMount () {
+    this.setState({
+      active_pitches: this.getActivePitches(
+        this.state.pitch_name,
+        this.state.key_mode
+      )
+    })
+  }
+
+  getActivePitches (pitchName, formula) {
+    let pitches = this.getPitchArray(pitchName)
+    let activePitches = []
+    for (let i = 0; i < formula.items.length; i++) {
+      activePitches.push(pitches[formula.items[i]])
     }
+    return activePitches
+  }
+
+  getPitchArray (pitchName) {
+    let pitches = []
+    let limit = PITCH_NAMES.length
+    let j = PITCH_NAMES.indexOf(pitchName)
+    for (let i = 0; i < this.props.show_frets; i++) {
+      let num = i + j
+      if (num >= limit) num = num - limit
+      pitches.push(PITCH_NAMES[num])
+    }
+    return pitches
+  }
+
+  removeTuning (index) {
+    let tuning = this.state.tuning.slice()
+    tuning.splice(index, 1)
+    this.setState({tuning: tuning})
+  }
+
+  render () {
+    return (
+      <div id='fretscale-app'>
+        <Fretboard
+          app={this}
+          fretboard_title={
+            this.state.pitch_name + ' ' + this.state.key_mode.name
+          }
+        />
+        <KeyModeSettings app={this} />
+      </div>
+    )
+  }
 }
 
-
 ReactDOM.render(
-    <FretScaleApp
-        show_frets="13"
-    />,
-    document.getElementById('root')
-);
+  <FretScaleApp
+    show_frets='13'
+  />,
+  document.getElementById('root')
+)
